48352393fedd7d12972d1d4747bc0d7b243237c9
Saikat <saikatc@virginia.edu>
2019-02-26 13:47:52 -0500

    change edit script

--- run_edit.sh
+++ run_edit.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+src=$1
+tgt=$2
+model=$3
 
 python edit/train.py \
-	-save_path projects/ins_del_edit/model \
-	-log_home projects/ins_del_edit/model \
+	-save_path $model \
+	-log_home log_tmp \
 	-online_process_data \
-	-train_src test.src \
-	-train_tgt test.tgt \
+	-train_src $src \
+	-train_tgt $tgt \
 	-layers 1 -enc_rnn_size 512 -brnn -word_vec_size 300 -dropout 0.5 \
 	-batch_size 2 -beam_size 1 \
-	-epochs 20 \
+	-epochs 2000 \
 	-gpus 0 \
 	-optim adam -learning_rate 0.001 \
 	-curriculum 0 -extra_shuffle \
-	-start_eval_batch 15000 -eval_per_batch 1200 -seed 12345 -cuda_seed 12345 -log_interval 100
+	-seed 12345 -cuda_seed 12345
 
